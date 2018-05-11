5fc5d307299fbf565f12ddc9269d790562aee431
Atif Afzal <atif5801@gmail.com>
2018-05-11 15:47:40 +0530

    Electron starter

--- fuse.ts
+++ fuse.ts
@@ -1,18 +1,35 @@
+import { spawn } from 'child_process';
 import { FuseBox, WebIndexPlugin } from 'fuse-box';
 
 const fuse = FuseBox.init({
   homeDir: 'src/',
-  target: 'browser@es6',
   output: 'dist/$name.js',
-  plugins: [WebIndexPlugin()],
+  plugins: [WebIndexPlugin({ bundles: ['renderer/app'] })],
+  sourceMaps: true,
 });
 
-fuse.dev();
+fuse
+  .bundle('main/app')
+  .target('electron')
+  .instructions(' > [main/index.ts]')
+  .watch();
+
+fuse.dev({ port: 7979 });
 
 fuse
-  .bundle('app')
-  .instructions(' > index.tsx')
+  .bundle('renderer/app')
+  .target('browser@es6')
+  .instructions('> renderer/index.ts')
   .hmr()
   .watch();
 
-fuse.run();
+fuse.run().then(() => {
+  // startup electron
+  spawn('node', [`${__dirname}/node_modules/electron/cli.js`, __dirname], {
+    stdio: 'inherit',
+  }).on('exit', code => {
+    // tslint:disable-next-line:no-console
+    console.log(`electron process exited with code ${code}`);
+    process.exit(code);
+  });
+});
